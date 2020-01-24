8b2168e13c2d53a4ac5f2d34fdcf2f0b3c4816ea
Benjamin Nylund <zs9684se@go.minnstate.edu>
2020-01-24 11:00:37 -0600

    Part 1 Completed

--- GreenvilleRevenue/Program.cs
+++ GreenvilleRevenue/Program.cs
@@ -10,6 +10,18 @@ namespace GreenvilleRevenue
     {
         static void Main(string[] args)
         {
+            Console.Write("How many contestants entered last year? ");
+            int contestantsLast = int.Parse(Console.ReadLine());
+
+            Console.Write("How many contestants entered this year? ");
+            int contestantsNow = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine(" Contestants last year: " + contestantsLast);
+            Console.WriteLine(" Contestants this year: " + contestantsNow);
+            Console.WriteLine("-----------------------------");
+
+            // Revenue / contestant comparison here
         }
     }
 }
